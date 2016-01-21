86d0d8115cceacaacc8e16eaf2ffdd550d81903d
Matt Heard <matt.heard@powershop.co.nz>
2016-01-21 13:18:40 +1300

    Print hello world with ncurses

--- life.rb
+++ life.rb
@@ -1,3 +1,30 @@
 #! /usr/bin/env ruby
 
-puts "hello world"
+require 'ncurses'
+
+class Life
+  def initialise_screen
+    Ncurses.initscr
+  end
+
+  def print_hello_world
+    Ncurses.mvaddstr(4, 19, "Hello, world!");
+  end
+
+  def refresh
+    Ncurses.refresh
+  end
+
+  def close_screen
+    Ncurses.endwin
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  game = Life.new
+  game.initialise_screen
+  game.print_hello_world
+  game.refresh
+  sleep(2.5)
+  game.close_screen
+end
