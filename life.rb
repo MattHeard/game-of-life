bdf0aaa194edb916aa49457894818d6eef3d9187
Matt Heard <matt.heard@powershop.co.nz>
2016-01-21 13:30:53 +1300

    Fill the screen

--- life.rb
+++ life.rb
@@ -3,12 +3,22 @@
 require 'ncurses'
 
 class Life
-  def initialise_screen
-    Ncurses.initscr
+  PICTURE = [" ", "█"]
+  UNKNOWN_VALUE_PICTURE = "?"
+
+  WINDOW_HEIGHT = `/usr/bin/env tput lines`.to_i
+  WINDOW_WIDTH = `/usr/bin/env tput cols`.to_i
+
+  def initialise_grid
+    @grid = Array.new(WINDOW_HEIGHT, Array.new(WINDOW_WIDTH, 1))
   end
 
-  def print_hello_world
-    Ncurses.mvaddstr(4, 19, "Hello, world!");
+  def display_grid
+    @grid.each_with_index { |row, index| display_row(index, row) }
+  end
+
+  def initialise_screen
+    Ncurses.initscr
   end
 
   def refresh
@@ -18,13 +28,24 @@ class Life
   def close_screen
     Ncurses.endwin
   end
+
+  private
+
+  def display_row(index, row)
+    Ncurses.mvaddstr(index, 0, row.map { |cell| picture(cell) }.join)
+  end
+
+  def picture(cell)
+    PICTURE[cell] || UNKNOWN_VALUE_PICTURE
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
   game = Life.new
   game.initialise_screen
-  game.print_hello_world
+  game.initialise_grid
+  game.display_grid
   game.refresh
-  sleep(2.5)
+  sleep(2)
   game.close_screen
 end
